611eb32dc9ea11ade58eb1c884c9946f26f1e684
Christian Graf <christian.graf@cyberfox.at>
2019-05-16 16:48:26 +0200

    feat(helpers): Add some helper functions.
    * application_version()

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+if (function_exists('app')) {
+    if (!function_exists('application_version')) {
+        /**
+         * @return \Fox\Application\Version\Version
+         */
+        function application_version(): \Fox\Application\Version\Version
+        {
+            return app(\Fox\Application\Version\VersionManager::class)->getCurrentVersion();
+        }
+    }
+}
